5807113ab64b8431b2ef28ac9f470ef25e63be46
xavttt <159512398+xavttt@users.noreply.github.com>
2024-03-14 23:18:14 +0800

    half done login page

--- login.php
+++ login.php
@@ -4,34 +4,27 @@
 
 <body>
   <?php include "inc/nav.inc.php"; ?>
-    <main class="container">
-      <h1>
-        Member Registration
-      </h1>
-      <p>
-        For new members, go to here
-        <a href="register.php">
-          Register Page
-        </a>
-        .
-      </p>
+    <main class="loginbody">
+      <div class="logincard">
+        <h3>Student Login</h3>
+        <p>New? Sign up <a href=register.php>here</a>!</p>
 
-      <form action="process_login.php" method="post">
-       
-        <div class="mb-3">
-            <label for="email" class="form-label">Email:</label>
-            <input required maxlength="45" class="form-control" type="email" id="email" name="email" placeholder="Enter email">
-        </div>
+        <form action="process_login.php" method="post">
+            <div class="mb-3">
+                <label for="email" class="loginlabel">Email:</label>
+                <input required maxlength="45" class="form-control" type="email" id="email" name="email" placeholder="Enter email">
+            </div>
 
-        <div class="mb-3">
-            <label for="pwd" class="form-label">Password:</label>
-            <input required class="form-control" type="password" id="pwd" name="pwd" placeholder="Enter password">
-        </div>
-       
-        <div class="mb-3"> 
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-      </form>
+            <div class="mb-3">
+                <label for="pwd" class="loginlabel">Password:</label>
+                <input required class="form-control" type="password" id="pwd" name="pwd" placeholder="Enter password">
+            </div>
+          
+            <div class="mb-3"> 
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+      </div>
     </main>
     <?php include "inc/footer.inc.php"; ?>
 </body>
