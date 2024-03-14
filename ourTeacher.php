4965bc1aa4949f8749a23b451fb5951b3a29e6ef
Jagatees <140966272+Jagatees@users.noreply.github.com>
2024-03-14 23:04:23 +0800

    Add : Simple Card

--- ourTeacher.php
+++ ourTeacher.php
@@ -17,7 +17,43 @@
         ?>
 
         <main class="container">
-            
+
+
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                        <h3>Card 1</h3>
+                        <p>Some text</p>
+                        <p>Some text</p>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <h3>Card 2</h3>
+                        <p>Some text</p>
+                        <p>Some text</p>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <h3>Card 3</h3>
+                        <p>Some text</p>
+                        <p>Some text</p>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <h3>Card 4</h3>
+                        <p>Some text</p>
+                        <p>Some text</p>
+                    </div>
+                </div>
+            </div>
+
+
 
         </main>
         <?php
