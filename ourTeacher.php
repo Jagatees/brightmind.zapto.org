0732c530f0d4bc8fde1d3ca4a84b3fde7ed24a5b
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2024-03-15 11:02:11 +0800

    Refactor : Name

--- ourTeacher.php
+++ ourTeacher.php
@@ -21,13 +21,13 @@
             <div class="grid-container">
                 <div class="card">
                     <img src="img_avatar.png" alt="Avatar" style="width:100%">
-                    <div class="container">
+                    <div class="card-container">
                         <h4><b>John Doe</b></h4>
                         <p>Architect & Engineer</p>
                     </div>
                 </div>
-                <!-- Repeat the above div 8 more times to make a total of 9 cards -->
-                <!-- Make sure to change the contents or src of each card as needed -->
+
+               
             </div>
 
 
