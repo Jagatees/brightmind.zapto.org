ae82ce62347caec0a2dfaf3fc4bea7d977b70115
Jagatees <140966272+Jagatees@users.noreply.github.com>
2024-03-18 19:09:05 +0800

    Refactor : OurTeacher
    - Fix Nav bar and Page overlapping

--- database/function.php
+++ database/function.php
@@ -1,25 +1,7 @@
 
 <?php
-function getDbConnection()
-{
-    $config = parse_ini_file('/var/www/private/db-config.ini');
-    if (!$config) {
-        die("Failed to read database config file.");
-    }
 
-    $conn = new mysqli(
-        $config['servername'],
-        $config['username'],
-        $config['password'],
-        $config['dbname']
-    );
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    return $conn;
-}
+include "database/connect.php";
 
 
 function insertIntoTable($name, $age, $bio, $subject, $price) {
@@ -68,3 +50,6 @@ function getTeachers() {
 
     return $teachers;
 }
+
+
+?>
--- ourTeacher.php
+++ ourTeacher.php
@@ -1,38 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <title>Bright Minds Academy - About Us</title>
-    <?php
-    include "inc/head.inc.php"; 
-    include "database-file/db.php"; 
-    $teachers = getTeachers(); 
-    ?>
-</head>
+<div id="main">
 
-<body>
-    <?php
-    include "inc/header.inc.php"; 
-    ?>
+    <head>
+        <title>About Us</title>
 
-    <main class="container">
-        <div class="grid-container">
-            <?php foreach ($teachers as $teacher) : ?>
-                <div class="card">
-                    <img src="img_avatar.png" alt="Avatar" style="width:100%">
-                    <div class="card-container">
-                        <h4><b><?php echo htmlspecialchars($teacher['name']); ?></b></h4>
-                        <p>Subject: <?php echo htmlspecialchars($teacher['subject']); ?></p>
-                        <p>Age: <?php echo htmlspecialchars($teacher['age']); ?></p>
-                        <p>Bio: <?php echo htmlspecialchars($teacher['bio']); ?></p>
+        <?php
+        include "inc/head.inc.php";
+        include "database/function.php";
+        $teachers = getTeachers();
+        ?>
+    </head>
+
+    <body>
+        <?php
+        include "inc/header.inc.php";
+        ?>
+
+        <main class="container">
+            <div class="grid-container">
+                <?php foreach ($teachers as $teacher): ?>
+                    <div class="card">
+                        <img src="img_avatar.png" alt="Avatar" style="width:100%">
+                        <div class="card-container">
+                            <h4><b>
+                                    <?php echo htmlspecialchars($teacher['name']); ?>
+                                </b></h4>
+                            <p>Subject:
+                                <?php echo htmlspecialchars($teacher['subject']); ?>
+                            </p>
+                            <p>Age:
+                                <?php echo htmlspecialchars($teacher['age']); ?>
+                            </p>
+                            <p>Bio:
+                                <?php echo htmlspecialchars($teacher['bio']); ?>
+                            </p>
+                        </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </main>
+                <?php endforeach; ?>
+            </div>
+        </main>
+        <?php
+        include "inc/footer.inc.php";
+        ?>
+</div>
 
-    <?php include "inc/footer.inc.php"; // Include footer components 
-    ?>
 </body>
 
 </html>
