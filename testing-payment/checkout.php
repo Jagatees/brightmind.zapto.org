24ae5c6bcb3c9f7a77360fb9b768667c3b06e5a2
Jagatees <140966272+Jagatees@users.noreply.github.com>
2024-03-31 09:13:33 +0800

    init

---
+++ testing-payment/checkout.php
@@ -0,0 +1,39 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+$stripe_secret_key = "your Stripe secret key here";
+
+\Stripe\Stripe::setApiKey($stripe_secret_key);
+
+$checkout_session = \Stripe\Checkout\Session::create([
+    "mode" => "payment",
+    "success_url" => "http://localhost/success.php",
+    "cancel_url" => "http://localhost/index.php",
+    "locale" => "auto",
+    "line_items" => [
+        [
+            "quantity" => 1,
+            "price_data" => [
+                "currency" => "usd",
+                "unit_amount" => 2000,
+                "product_data" => [
+                    "name" => "T-shirt"
+                ]
+            ]
+        ],
+        [
+            "quantity" => 2,
+            "price_data" => [
+                "currency" => "usd",
+                "unit_amount" => 700,
+                "product_data" => [
+                    "name" => "Hat"
+                ]
+            ]
+        ]        
+    ]
+]);
+
+http_response_code(303);
+header("Location: " . $checkout_session->url);
