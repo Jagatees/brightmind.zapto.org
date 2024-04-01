ede97479f55a40195ca296248677beb636bca39b
Jagatees <140966272+Jagatees@users.noreply.github.com>
2024-04-02 03:01:49 +0800

    Revert Back to "Testing Code : PENDING"

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,15 +1,11 @@
 <header class="jumbotron text-dark custom-bg ">
-    <div style="width:30%; text-align:left;">
-        <?php include "components/nav-bar/nav.php"; ?>
-    </div>
-    <div class="header-title" style="padding-top:10px; padding-bottom:10px; width:40%;">
+    <?php include "components/nav-bar/nav.php"; ?>
+    <div class="header-title" style="padding-top:10px; padding-bottom:10px; padding-right:25px;">
         <img src="images/BMALogo2.png" alt="" height="80px" width="262px">
     </div>
-    <div style="width:30%; text-align:right; padding-right:5px;">
-        <?php if(isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in']): ?>
-            <span style="font-size:15px; cursor:pointer; padding-left:5px; color:#ffffff;" >Welcome Back, <?php echo htmlspecialchars($_SESSION['fname']) . ' ' . htmlspecialchars($_SESSION['lname']) . ' ( Role :' . htmlspecialchars(ucfirst($_SESSION['role'])) . ')'; ?></span>
-        <?php else: ?>
-            <a href="login.php" style="font-size:15px; cursor:pointer; padding-left:5px; color:#ffffff; text-decoration: none;">Not Logged In</a>
-        <?php endif; ?>
-    </div>
+    <?php if(isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in']): ?>
+        <span style="font-size:15px; cursor:pointer; padding-left:5px; color:#ffffff;" >Welcome Back, <?php echo htmlspecialchars($_SESSION['uuid']) . ' ' . htmlspecialchars($_SESSION['lname']) . ' ( Role :' . htmlspecialchars(ucfirst($_SESSION['role'])) . ')'; ?></span>
+    <?php else: ?>
+        <a href="login.php" style="font-size:15px; cursor:pointer; padding-left:5px; color:#ffffff; text-decoration: none;">Not Logged In</a>
+    <?php endif; ?>
 </header>
